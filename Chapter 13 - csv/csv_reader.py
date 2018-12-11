4c3f0ed35614a055b81b1bcca080127f776c50f0
Mike Driscoll <mike@pythonlibrary.org>
2018-12-11 07:59:41 -0600

    Fixed typo for Python 3

--- Chapter 13 - csv/csv_reader.py
+++ Chapter 13 - csv/csv_reader.py
@@ -11,5 +11,5 @@ def csv_reader(file_obj):
     
 if __name__ == "__main__":
     csv_path = "TB_data_dictionary_2014-02-26.csv"
-    with open(csv_path, "rb") as f_obj:
+    with open(csv_path, "r") as f_obj:
         csv_reader(f_obj)
