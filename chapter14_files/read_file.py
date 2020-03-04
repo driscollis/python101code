8272a8595095b6fac3fc3e58c4f77c909c51078c
Mike Driscoll <mike@pythonlibrary.org>
2020-03-04 09:56:52 -0600

    add example code for chapter 14

---
+++ chapter14_files/read_file.py
@@ -0,0 +1,4 @@
+file_handler = open('example.txt')
+for line in file_handler:
+    print(line)
+file_handler.close()
---
+++ chapter14_files/read_file_with.py
@@ -0,0 +1,3 @@
+with open('example.txt') as file_handler:
+    for line in file_handler:
+        print(line)
