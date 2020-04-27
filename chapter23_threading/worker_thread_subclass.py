ed304b179b4e82544c3c64e118408ff79065a2d0
Mike Driscoll <mike@pythonlibrary.org>
2020-04-27 10:02:03 -0500

    Make threading subclass more interesting

--- chapter23_threading/worker_thread_subclass.py
+++ chapter23_threading/worker_thread_subclass.py
@@ -9,18 +9,20 @@ class WorkerThread(threading.Thread):
     def __init__(self, name):
         threading.Thread.__init__(self)
         self.name = name
+        self.id = id(self)
 
     def run(self):
         """
         Run the thread
         """
-        worker(self.name)
+        worker(self.name, self.id)
 
-def worker(name: str) -> None:
-    print(f'Started worker {name}')
+def worker(name: str, instance_id: int) -> None:
+    print(f'Started worker {name} - {instance_id}')
     worker_time = random.choice(range(1, 5))
     time.sleep(worker_time)
-    print(f'{name} worker finished in {worker_time} seconds')
+    print(f'{name} - {instance_id} worker finished in '
+          f'{worker_time} seconds')
 
 if __name__ == '__main__':
     for i in range(5):
