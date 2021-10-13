a8a264150bef6d84e984f9228bfa9f9b1adf3748
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-13 16:32:21 +0800

    Delete chapter18_classes directory

--- chapter18_classes/simple_ball.py
+++
@@ -1,7 +0,0 @@
-class Ball:
-
-    def __init__(self, color, size, weight):
-        """Constructor"""
-        self.color = color
-        self.size = size
-        self.weight = weight
--- chapter18_classes/simple_ball_type_hints.py
+++
@@ -1,6 +0,0 @@
-class Ball:
-
-     def __init__(self, color: str, size: float, weight: float) -> None:
-          self.color = color
-          self.size = size
-          self.weight = weight
