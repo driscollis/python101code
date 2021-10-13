ba9bc1c389374762723978e04c210f9441c190ca
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-13 16:30:13 +0800

    Delete chapter12_loops directory

--- chapter12_loops/nested_loops.py
+++
@@ -1,5 +0,0 @@
-nested = [['mike', 12], ['jan', 15], ['alice', 8]]
-for lst in nested:
-    print(f'List = {lst}')
-    for item in lst:
-        print(f'Item -> {item}')
--- chapter12_loops/while_with_break.py
+++
@@ -1,7 +0,0 @@
-count = 0
-while count < 10:
-    if count == 4:
-        print(f'count={count}')
-        break
-    print(count)
-    count += 1
