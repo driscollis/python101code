d3024cf7c1f7846e11dace3ab036ada4699d8ce6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-13 16:32:06 +0800

    Delete chapter14_files directory

--- chapter14_files/read_file.py
+++
@@ -1,4 +0,0 @@
-file_handler = open('example.txt')
-for line in file_handler:
-    print(line)
-file_handler.close()
--- chapter14_files/read_file_with.py
+++
@@ -1,3 +0,0 @@
-with open('example.txt') as file_handler:
-    for line in file_handler:
-        print(line)
